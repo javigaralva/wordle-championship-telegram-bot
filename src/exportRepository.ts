e0dc8aebf96b8e0af03f95ecdc8c693c04ff4ac6
javigaralva <javigaralva@gmail.com>
2022-03-28 19:10:22 +0200

    feat: add import/export repository utilites

--- src/exportRepository.ts
+++ src/exportRepository.ts
@@ -10,5 +10,7 @@ import fs from 'fs/promises'
 
     const json = JSON.stringify( { players, championshipResults }, null, 2 )
     const date = new Date().toISOString().split( '.' )[ 0 ].replace( /[^\d]/gi, '' )
-    await fs.writeFile( `./data_${date}.json`, json )
+    const dbFileName = `./DB_backup/data_${date}.json`
+    await fs.writeFile( dbFileName, json, 'utf8' )
+    console.log( `✅ DB backup created (${dbFileName})` )
 } )()
