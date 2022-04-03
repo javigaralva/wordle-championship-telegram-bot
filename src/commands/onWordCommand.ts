3fe854446e530b75a0d54cefb6b2ae13699681fe
javigaralva <javigaralva@gmail.com>
2022-04-03 20:25:22 +0200

    feat: protect some commands to admin user

--- src/commands/onWordCommand.ts
+++ src/commands/onWordCommand.ts
@@ -1,11 +1,15 @@
 import TelegramBot from 'node-telegram-bot-api'
 import { sendMessage } from '../bot/sendMessage'
+import { ADMIN_ID } from '../config/config'
 import { addWord } from '../services/admin'
 import { getTodaysGameId } from '../services/gameUtilities'
 
 export const onWordCommandRegex = /#word ((?<gameId>\d+) (?<wordGameId>[a-zñáéíóú]+)|(?<word>[a-zñáéíóú]+))/gm
 
 export async function onWordCommandHandler( msg: TelegramBot.Message ) {
+
+    if( ADMIN_ID !== msg.chat.id ) return
+
     const match = msg.text?.matchAll( onWordCommandRegex )
     if( !match ) return
 
