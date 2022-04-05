8bf3d471213cbaa461c3e4923e2aa04666b935ac
javigaralva <javigaralva@gmail.com>
2022-04-05 19:26:44 +0200

    feat: add users notification when player played

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,3 +1,4 @@
 export const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN
 export const ALL_PLAYERS_IDS = JSON.parse( process.env.ALL_PLAYERS_IDS ?? '[]' )
 export const ADMIN_ID = Number( process.env.ADMIN_ID )
+export const NOTIFICATION_PLAYERS_IDS = JSON.parse( process.env.NOTIFICATION_PLAYERS_IDS ?? '[]' )
