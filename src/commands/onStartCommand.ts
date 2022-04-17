f63eb85b933ff14d23742e0f6bd59ffc7ddd20ac
javigaralva <javigaralva@gmail.com>
2022-04-17 22:24:54 +0200

    feat: change normal mode to accent in start message

--- src/commands/onStartCommand.ts
+++ src/commands/onStartCommand.ts
@@ -5,8 +5,10 @@ export const onStartCommandRegex = /\/start/
 
 export async function onStartCommandHandler( msg: TelegramBot.Message ) {
     const text =
-        `*Hola ${msg.from?.first_name ?? 'personaje misterioso'} 👋 ¡Bienvenido a Wordle Championship!*\n` +
+        `*Hola ${msg.from?.first_name ?? 'personaje misterioso'} 👋 ¡Bienvenido a Wordle Tildes Championship!*\n` +
+        //`*Hola ${msg.from?.first_name ?? 'personaje misterioso'} 👋 ¡Bienvenido a Wordle Tildes Championship!*\n` +
         '🏁 Cada lunes comienza automáticamente un nuevo campeonato.\n' +
-        '📨 Para participar solo me tienes que reenviar el resultado desde la web de https://wordle.danielfrg.com cuando termines la partida.\n'
+        '📨 Para participar solo me tienes que reenviar el resultado desde la web de https://wordle.danielfrg.com (versión *TILDES*) cuando termines la partida.\n'
+        // '📨 Para participar solo me tienes que reenviar el resultado desde la web de https://wordle.danielfrg.com cuando termines la partida.\n'
     await sendMessage( msg.chat.id, text )
 }
