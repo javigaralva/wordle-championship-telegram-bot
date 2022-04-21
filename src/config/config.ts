c34b70d1836f7f920320c3e38020439e61f868be
javigaralva <javigaralva@gmail.com>
2022-04-21 17:56:14 +0200

    feat: add wordle types

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,4 +1,7 @@
+type WordleTypes = 'NORMAL' | 'ACCENT' | 'SCIENCE'
+
 export const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN
 export const ALL_PLAYERS_IDS = JSON.parse( process.env.ALL_PLAYERS_IDS ?? '[]' )
 export const ADMIN_ID = Number( process.env.ADMIN_ID )
 export const NOTIFICATION_PLAYERS_IDS = JSON.parse( process.env.NOTIFICATION_PLAYERS_IDS ?? '[]' )
+export const WORDLE_TYPE: WordleTypes = process.env.WORDLE_TYPE as WordleTypes ?? 'NORMAL'
