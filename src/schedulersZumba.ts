7ff2018139c7859cda8ba157dde69f9801af6fbf
javigaralva <javigaralva@gmail.com>
2022-11-15 00:20:03 +0100

    fix: change node-cron due to high mem resources consume

--- src/schedulersZumba.ts
+++ src/schedulersZumba.ts
@@ -1,25 +1,25 @@
-import cron from 'node-cron'
+import { CronJob } from 'cron'
 import { dispatchGithubWorkflow, ZumbaCommand } from './services/zumba'
 
 const TIMEZONE = 'Europe/Madrid'
 
 export function scheduleZumbaVina() {
     {
-        const cronExpression = '0 18 * * 0'
+        const cronExpression = '0 0 18 * * 0'
         console.log(`${new Date().toISOString()} >> Scheduling Zumba Vina (Sunday at 18:00) with this pattern: '${cronExpression}'`)
-        cron.schedule(cronExpression, handleVinaReservation, { timezone: TIMEZONE })
+        new CronJob(cronExpression, handleVinaReservation, null, true, TIMEZONE )
     }
     {
-        const cronExpression = '0 19 * * 2'
+        const cronExpression = '0 0 19 * * 2'
         console.log(`${new Date().toISOString()} >> Scheduling Zumba Vina (Tuesday at 19:00) with this pattern: '${cronExpression}'`)
-        cron.schedule(cronExpression, handleVinaReservation, { timezone: TIMEZONE })
+        new CronJob(cronExpression, handleVinaReservation, null, true, TIMEZONE)
     }
 }
 
 export function scheduleZumbaDehesa() {
-    const cronExpression = '0 22 * * 0'
+    const cronExpression = '0 0 22 * * 0'
     console.log(`${new Date().toISOString()} >> Scheduling Zumba Dehesa (Sunday at 22:00) with this pattern: '${cronExpression}'`)
-    cron.schedule(cronExpression, handleDehesaReservation, { timezone: TIMEZONE })
+    new CronJob(cronExpression, handleDehesaReservation, null, true, TIMEZONE)
 }
 
 async function handleVinaReservation() {
