24b648d03ffcdb6b745610dc42ec43a30ca82703
javigaralva <javigaralva@gmail.com>
2022-12-12 17:54:56 +0100

    fix: add try/catch for RAE and google services calls

--- src/services/googleDefinitions.ts
+++ src/services/googleDefinitions.ts
@@ -5,32 +5,36 @@ const google = require( 'googlethis' )
 export const getGoogleDefinitionsAndExamplesFor = memoizeAsync( async function ( word: string ) {
     if( !word ) return
 
-    const response = await google.search( `${word} definición`, {
-        page: 0,
-        safe: false,
-        additional_params: {
-            hl: 'es'
-        }
-    } )
+    try {
+        const response = await google.search( `${word} definición`, {
+            page: 0,
+            safe: false,
+            additional_params: {
+                hl: 'es'
+            }
+        } )
 
-    const definitions = ( response?.dictionary?.definitions ?? [] )
-        .map( ( definition: string, i: number ) => `   ${i % 2 ? '🔶' : '🔷'} ${markdownEscape( definition )}` )
-        .flat()
-        .join( '\n' )
+        const definitions = ( response?.dictionary?.definitions ?? [] )
+            .map( ( definition: string, i: number ) => `   ${i % 2 ? '🔶' : '🔷'} ${markdownEscape( definition )}` )
+            .flat()
+            .join( '\n' )
 
-    const examples = ( response?.dictionary?.examples ?? [] )
-        .map( ( example: string, i: number ) => `   🗣 ${markdownEscape( example )}` )
-        .flat()
-        .join( '\n' )
+        const examples = ( response?.dictionary?.examples ?? [] )
+            .map( ( example: string, i: number ) => `   🗣 ${markdownEscape( example )}` )
+            .flat()
+            .join( '\n' )
 
-    const wordUpperCase = word.toUpperCase()
-    const text = [
-        definitions && `✍️ Definición de *${wordUpperCase}*`,
-        definitions,
-        definitions && ' ',
-        examples && `💬 Ejemplos de *${wordUpperCase}*`,
-        examples
-    ].filter( Boolean ).join( '\n' )
-
-    return text
+        const wordUpperCase = word.toUpperCase()
+        const text = [
+            definitions && `✍️ Definición de *${wordUpperCase}*`,
+            definitions,
+            definitions && ' ',
+            examples && `💬 Ejemplos de *${wordUpperCase}*`,
+            examples
+        ].filter( Boolean ).join( '\n' )
+        return text
+    }
+    catch (error) {
+        return console.error( `Error searching in google for word ${word}: `, error )
+    }
 } )
--- src/services/raeDefinitions.ts
+++ src/services/raeDefinitions.ts
@@ -9,20 +9,24 @@ export const getRaeDefinitions = memoizeAsync( async function( word: string ) {
 
     if( !word ) return
 
-    const response = await raejs.search( word )
-    if( response.error ) return
+    try {
+        const response = await raejs.search( word )
+        if( response.error ) return
 
-    const text = he.decode( response.results.map( ( result: any, i: number ) =>
-        [
-            `👉 ${DEFINITION_ICONS[ i % DEFINITION_ICONS.length ]} *${markdownEscape( result.header )}* ${result.source ? `(${markdownEscape( result.source )})` : ''}`,
-            ...result.definition.map( ( def: string, i: number ) =>
-                `  ${BULLETS_ICONS[ i % BULLETS_ICONS.length ]}  ${markdownEscape( def )}`
-            ),
-            ''
-        ] )
-        .flat()
-        .join( '\n' )
-    )
-
-    return text
+        const text = he.decode( response.results.map( ( result: any, i: number ) =>
+            [
+                `👉 ${DEFINITION_ICONS[ i % DEFINITION_ICONS.length ]} *${markdownEscape( result.header )}* ${result.source ? `(${markdownEscape( result.source )})` : ''}`,
+                ...result.definition.map( ( def: string, i: number ) =>
+                    `  ${BULLETS_ICONS[ i % BULLETS_ICONS.length ]}  ${markdownEscape( def )}`
+                ),
+                ''
+            ] )
+            .flat()
+            .join( '\n' )
+        )
+        return text
+    }
+    catch (error) {
+        return console.error( `Error searching in rae for word ${word}: `, error )
+    }
 } )
